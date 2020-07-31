72739e6863c1d0c27571b3e28e1a19e267180dad
mariechu3 <mariechu3@gmail.com>
2020-07-31 15:03:49 -0700

    starting to figure out how to add a new column

---
+++ content.js
@@ -0,0 +1,17 @@
+
+// 1. Create the button
+var button = document.createElement("button");
+button.innerHTML = "Do Something";
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.insertBefore(button, document.body.firstChild);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+  alert("did something");
+});
+
+var row = document.getElementById("ui-jqgrid-labels ui-sortable");
+var x = row.insertCell(0);
+x.innerHTML = "New cell";
